735a0e1797fc80dcbac3875a3ed61373ca425231
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-11-29 12:41:54 +0900

    first

---
+++ app/models/tweet.rb
@@ -0,0 +1,11 @@
+class Tweet < ApplicationRecord
+  validates :text, presence: true
+
+  belongs_to :user
+  belongs_to :comment
+
+  def self.search(search)
+    return Tweet.all unless search
+    Tweet.where('text LIKE(?)', "%#{search}%")
+  end
+end
